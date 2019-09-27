95bac02141dea172cf2818331e05edf155c3d824
kirito <raokarthik83@gmail.com>
2019-09-27 16:38:35 +0530

    Advanced functions Completed

--- src/advancedFunctions.kt
+++ src/advancedFunctions.kt
@@ -18,17 +18,73 @@ class  ClassB {
 }
 
 
+/**
+ * For us to invoke the toString() method of the dispatch receiver ClassA instead, we need to use a qualified this
+ *
+ */
+
 class  ClassA {
     fun ClassB.exFuntion() {
         print(toString()) // calls ClassB toString()
+        println(this@ClassA.toString())
     }
 
     fun callExFunction(classB: ClassB){
         classB.exFuntion()  // call the extension function
     }
+
+}
+
+/**
+ * Higher_Order Functions
+ */
+
+// A higher order function is a function which takes another another function as a parameter, returns a function or does both
+
+/**
+ * Creating higher order function
+ */
+fun calCircumference2(radius: Double) = (2 * Math.PI) * radius
+
+fun calArea(radius: Double): Double = (Math.PI) * Math.pow(radius, 2.0)
+
+fun circleOperation(radius: Double, op: (Double) -> Double): Double{
+    val result = op(radius)
+    return result
+}
+
+/**
+ * Returning a function
+ */
+
+fun multiplier(factor: Double): (Double) -> Double = {
+    number -> number*factor
+}
+
+/**
+ * Closures
+ */
+
+// A closure is a function that has access to variables and parameters which are defined in a n outer scope
+
+fun printFilteredNamesByLength(length: Int){
+    val names = arrayListOf("Adam", "Andrew", "Chike", "Kechi")
+    val filterResult = names.filter {
+        it.length == length
+    }
+    println(filterResult)
 }
 
 
+/**
+ * Inline functions
+ */
+
+fun circleOperation1(radius: Double, op: (Double) -> Double){
+    println("Radius is $radius")
+    val result = op (radius)
+    println("The result is $result")
+}
 
 fun main() {
 
@@ -36,5 +92,26 @@ fun main() {
     println(firstName.upperCaseFirstLetter())
     println(firstName)
 
-    
+    println(circleOperation(2.0, ::calArea))
+    println(circleOperation(4.0, ::calCircumference2))
+
+
+    /**
+     *
+     * We can also pass lambdas to higher order functions
+     */
+
+    // Pass lambdas to functions
+    circleOperation(5.0, {
+        (2 * Math.PI) * it
+    })
+
+    var doubler = multiplier(2.0)
+    println(doubler(5.6))
+
+    printFilteredNamesByLength(5)
+
+    circleOperation1(5.3, {
+        (2 * Math.PI) * it
+    })
 }
